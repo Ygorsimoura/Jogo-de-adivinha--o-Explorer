c28d99e858e3d7dae15788e98c7c8ab721017960
Ygor Simoura <ygorsimoura01@gmail.com>
2023-05-31 08:37:55 -0300

    $commit

---
+++ main.js
@@ -0,0 +1,45 @@
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+let randomNumber = Math.round(Math.random() * 10)
+let xAttemps = 1
+
+
+//Evenetos
+
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click',handleResetClick)
+document.addEventListener('keydown', function(e) {
+    if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+        handleResetClick()
+    }
+})
+
+// funções callback
+
+function handleTryClick(event){
+    event.preventDefault()
+    
+    const inputNumber = document.querySelector("#inputNumber")
+
+    if(Number(inputNumber.value) == randomNumber ) {
+        toggleScreen()
+        screen2.querySelector("h2").innerText = `Você acertou em ${xAttemps} Tentativas` 
+        
+    } 
+
+    inputNumber.value = ''
+    xAttemps++
+}
+
+function handleResetClick() {
+    toggleScreen()
+    xAttemps = 1
+    randomNumber = Math.round(Math.random() * 10)
+}
+
+function toggleScreen() {
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+}
